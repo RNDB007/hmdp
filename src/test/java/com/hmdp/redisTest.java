0e205dd787995e6e193063b4f477b5863d6700d9
li <a374859311@gmail.com>
2023-11-07 01:30:40 +0800

    第一次测试类正常运行

---
+++ src/test/java/com/hmdp/redisTest.java
@@ -0,0 +1,22 @@
+package com.hmdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @author Joe Lee
+ * @date
+ * @Description
+ */
+@SpringBootTest
+public class redisTest {
+    @Autowired
+    RedisTemplate redisTemplate;
+    @Test
+    void testString(){
+        redisTemplate.opsForValue().set("name","li");
+        System.out.println(redisTemplate.opsForValue().get("name"));
+    }
+}
