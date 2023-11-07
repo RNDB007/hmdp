23c1bd1613295c683249fb2af23bbc4195cd24a3
li <a374859311@gmail.com>
2023-11-07 21:45:24 +0800

    hash测试正常运行

--- src/test/java/com/hmdp/redisTest.java
+++ src/test/java/com/hmdp/redisTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * @author Joe Lee
@@ -14,10 +15,20 @@ import org.springframework.data.redis.core.RedisTemplate;
 public class redisTest {
     @Autowired
     RedisTemplate redisTemplate;
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
     @Test
     void testString(){
 
-        System.out.println(redisTemplate.opsForValue().get("name"));
+       stringRedisTemplate.opsForValue().set("name","joe");
+         System.out.println(stringRedisTemplate.opsForValue().get("name"));
 
+    }
+    @Test
+    void testHash(){
+        stringRedisTemplate.opsForHash().put("user","name","joe");
+        stringRedisTemplate.opsForHash().entries("user").forEach((k,v)->{
+            System.out.println(k+" "+v);
+        });
     }
 }
